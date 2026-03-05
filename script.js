345ae116612b2a88f072d69622ca2923c48c4faa
Sarthak0009-pro <r9375639@gmail.com>
2026-03-05 21:43:13 +0530

    Fix timer logic and improve sound playback

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ let secondsLeft = 0
 let timer = null
 
 function startTimer(){
+
+let min = document.getElementById("minutes").value
+
+if(secondsLeft === 0){
+if(min <= 0) return
+totalSeconds = min * 60
+secondsLeft = totalSeconds
+}
+
 clearInterval(timer)
 
-timer=setInterval(()=>{
+timer = setInterval(()=>{
 
 secondsLeft--
+
 updateDisplay()
 
 let percent=((totalSeconds-secondsLeft)/totalSeconds)*100
@@ -15,16 +25,22 @@ document.getElementById("bar").style.width=percent+"%"
 
 if(secondsLeft<=0){
 clearInterval(timer)
+playSound()
 }
 
 },1000)
+
+}
+
+function pauseTimer(){
+clearInterval(timer)
 }
 
 function resetTimer(){
 clearInterval(timer)
-secondsLeft=totalSeconds
-updateDisplay()
+secondsLeft=0
 document.getElementById("bar").style.width="0%"
+updateDisplay()
 }
 
 function updateDisplay(){
@@ -37,3 +53,10 @@ sec=sec<10?"0"+sec:sec
 
 document.getElementById("time").innerText=min+":"+sec
 }
+
+function playSound(){
+let audio=new Audio("https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg")
+audio.play()
+}
+
+updateDisplay()
